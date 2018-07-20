a832b84017536ad665160f2e3da02d86c1ce2d36
Joe Quesada <jquesada217@users.noreply.github.com>
2018-07-20 16:50:24 -0400

    implementation for Pencil.write

--- src/main/java/jq/tddpencil/Pencil.java
+++ src/main/java/jq/tddpencil/Pencil.java
@@ -1,7 +1,24 @@
 package jq.tddpencil;
 
 public class Pencil {
-    public Pencil() {
 
+    // Private state
+    private String text;
+
+    // Constructors
+    public Pencil() {
+        text = "";
+    }
+
+    // Property accessors
+    public String getText() {
+        return text;
+    }
+
+    // Instance methods
+    public void write(String textToWrite) {
+        StringBuilder sb = new StringBuilder(text);
+        sb.append(textToWrite);
+        text = sb.toString();
     }
 }
