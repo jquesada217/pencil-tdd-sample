71d1ec2410578a2fd06a81b2e4f309e7ea67b5c8
Joe Quesada <jquesada217@users.noreply.github.com>
2018-07-20 17:03:47 -0400

    Pencil.writeNewNote test spec

--- src/test/java/jq/tddpencil/PencilTest.java
+++ src/test/java/jq/tddpencil/PencilTest.java
@@ -8,6 +8,20 @@ import static org.junit.Assert.*;
 public class PencilTest {
 
     @Test
+    public void writeNewNote_writesNewText() {
+        // Arrange
+        Pencil pencil = new Pencil();
+        String text = "foo bar";
+
+        // Act
+        pencil.writeNewNote(text);
+        String actualText = pencil.getText();
+
+        // Assert
+        assertThat(actualText, is(text));
+    }
+
+    @Test
     public void write_appendsToWrittenText() {
         // Arrange
         Pencil pencil = new Pencil();
